5b90e4c0fc85b3976888559da31066fdacc3067b
dalalbn <148500813+dalalbn@users.noreply.github.com>
2024-03-27 02:40:09 +0100

    Add files via upload

---
+++ PMT_R-D/profil.js
@@ -0,0 +1,18 @@
+const imgDiv = document.querySelector('.img'); 
+const img = document.querySelector('#photo'); 
+const file = document.querySelector('#file'); 
+const uploadbtn = document.querySelector('#uploadbtn');
+
+
+file.addEventListener('change', function(){
+const chosedfile = this.files[0] ; 
+if(chosedfile){
+    const reader = new FileReader();
+    reader.addEventListener('load' , function() {
+        img.setAttribute('src', reader.result);
+    })
+    reader.readAsDataURL(chosedfile); 
+}
+
+
+})
