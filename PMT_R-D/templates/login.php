674f04903baa1fce1859d3ae60cfd3a64c72aa16
khaledadk <148718060+khaledadk@users.noreply.github.com>
2024-03-27 17:28:09 -0700

    Add files via upload

---
+++ PMT_R-D/templates/login.php
@@ -0,0 +1,67 @@
+<?php
+session_start();
+if (isset($_SESSION["user"])) {
+   header("Location: index.php");
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+    <link rel="stylesheet" href="../static/css/pa.css">
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+</head>
+<body>
+    <div class="container">
+        <?php
+        if (isset($_POST["login"])) {
+           $email = $_POST["email"];
+           $password = $_POST["password"];
+            require_once "database.php";
+            $sql = "SELECT * FROM users WHERE email = '$email'";
+            $result = mysqli_query($conn, $sql);
+            $user = mysqli_fetch_array($result, MYSQLI_ASSOC);
+            if ($user) {
+                if (password_verify($password, $user["password"])) {
+                    session_start();
+                    $_SESSION["user"] = "yes";
+                    header("Location: index.php");
+                    die();
+                }else{
+                    echo "<div class='alert alert-danger'>Password does not match</div>";
+                }
+            }else{
+                echo "<div class='alert alert-danger'>Email does not match</div>";
+            }
+        }
+        ?>
+        <center>
+     <div class="WRAP">
+        <form action="login.php" method="post">
+        <h1>Connexion</h1>
+          <div class="input-box">
+              <input type="email" placeholder="Enter Email:" name="email" class="form-control">
+              <i class='bx bxs-user'></i> 
+          </div>
+          <div class="input-box">
+              <input type="password" placeholder="Enter Password:" name="password" class="form-control">
+              <i class='bx bxs-lock-alt' ></i> 
+          </div>
+          <div class="input-box">
+              <input type="submit" value="Login" name="login" class="btn">
+          </div>
+
+          
+          
+          <div class="tf"  > <p> <b>Not registered yet</b><a href="registration.php">Register Here</a></p></div>
+
+          </form>
+      </div> 
+       </center>
+    </div>
+</body>
+</html>
